db33a1e3328b8da57422a6dd223a6a3371b1850d
Koz <kenneth.averna@gmail.com>
2023-11-29 18:34:07 -0800

    added title to home page

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ function App() {
         </svg>
       </div>
 
+      <h1 className='absolute font-bold text-[7rem] top-1/2 left-1/2 transform -translate-x-96 -translate-y-72 text-[#146C94]'>Kenny & Aira</h1>
       <img className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-44" width="500px" height="auto" src="./Intersect.png" alt="cats" />
       <div className={`${menu} transition-all duration-700 ease-out-in`}>
         <Header />
