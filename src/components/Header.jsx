1a4e6ee765b92595f5fd16e4931e342fefb3b9b9
Koz <kenneth.averna@gmail.com>
2023-11-29 17:34:59 -0800

    animated the menu to slide in and out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 export default function Header() {
    return (
-      <div className='font-bold text-7xl p-10'>
-         <div className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Home</div>
-         <div className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Photo Gallery</div>
-         <div className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>About</div>
-         <div className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Extra</div>
+      <div className='flex flex-col font-bold text-7xl p-10 text-[#146C94]'>
+         <div>
+         <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Home</span>
+         </div>
+         <div>
+         <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Photo Gallery</span>
+         </div>
+         <div>
+         <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>About</span>
+         </div>
+         <div>
+         <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Extra</span>
+         </div>
       </div>
    );
 }
