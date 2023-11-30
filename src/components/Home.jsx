284d82299598255830eac7779934e7fa56108eff
Koz <kenneth.averna@gmail.com>
2023-11-29 23:03:50 -0800

    added a new title and some confetti

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import ConfettiExplosion from 'react-confetti-explosion';
 
 export default function Home() {
+   const [explosionKey, setExplosionKey] = React.useState(0);
+
+  function handleClick() {
+    // Increment the explosion key to force re-rendering of ConfettiExplosion
+    setExplosionKey((prevKey) => prevKey + 1);
+  }
+
+
    return (
       <main className="flex flex-col items-center justify-center h-screen">
-         <h1 className='font-bold text-7xl text-[#146C94]'>Kenny & Aira</h1>
+         <h1 className='font-bold text-6xl text-[#AFD3E2]'>Happy Birthday</h1>
+         <h1 className='font-bold text-4xl text-[#146C94]'>Christineil Aira Grace Catherine Lagare Berame Courselle</h1>
+         <ConfettiExplosion key={explosionKey} />
          <img className="mt-4" width="500px" height="auto" src="./Intersect.png" alt="cats" />
+         <button 
+           onClick={handleClick}
+            className='absolute -translate-x-4 translate-y-6 transition ease-in-out delay-150 hover:translate-y-4 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>
+            Press me
+            </button>
       </main>
    )
 }
--- src/components/People.jsx
+++ src/components/People.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Link } from "react-router-dom"
 
 export default function People() {
+
+  const style = 'transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'
   return (
     <div className='grid grid-cols-4 grid-flow-row gap-4 text-3xl mt-12 text-[#19A7CE] z-100'>
-      <Link to="/kenny"  className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Kenny</Link>
-      <Link to="/sarah"  className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Sarah</Link>
-      <Link to="/andrew" className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Andrew</Link>
-      <Link to="/nick"   className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Nick</Link>
-      <Link to="/nique"  className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Nique</Link>
-      <Link to="/pablo"  className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Pablo</Link>
-      <Link to="/megan"  className='transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 bg-[#AFD3E2] rounded-md p-2 flex items-center justify-center'>Megan</Link>
+      <Link to="/kenny"  className={`${style}`}>Kenny</Link>
+      <Link to="/sarah"  className={`${style}`}>Sarah</Link>
+      <Link to="/andrew" className={`${style}`}>Andrew</Link>
+      <Link to="/nick"   className={`${style}`}>Nick</Link>
+      <Link to="/nique"  className={`${style}`}>Nique</Link>
+      <Link to="/pablo"  className={`${style}`}>Pablo</Link>
+      <Link to="/megan"  className={`${style}`}>Megan</Link>
     </div>
   )
 }
