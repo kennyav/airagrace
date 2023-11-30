efff5278854499826063374c7f79c16ef5f9ae21
Koz <kenneth.averna@gmail.com>
2023-11-29 20:20:30 -0800

    nav in front, periphirals are changed, and added to photo gallery

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom"
 
-export default function Header() {
+export default function Header({setMenu}) {
+
+   function handleClick() {
+      setMenu(`-translate-x-full`)
+   }
    return (
       <nav className='flex flex-col font-bold text-7xl p-10 text-[#146C94]'>
-         <Link to="/" >
-            <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Home</span>
+         <Link to="/" onClick={handleClick}>
+            <span  className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Home</span>
          </Link>
-         <Link to="/photo-gallery">
+         <Link to="/photo-gallery" onClick={handleClick}>
             <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Photo Gallery</span>
          </Link>
-         <Link to="/about">
+         <Link to="/about" onClick={handleClick}>
             <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>About</span>
          </Link>
-         <Link to="/extra">
+         <Link to="/extra" onClick={handleClick}>
             <span className='transition-all duration-300 ease-in-out hover:text-9xl cursor-pointer'>Extra</span>
          </Link>
       </nav>
--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
+// photos components
+import People from './Photos/People'
+
 export default function PhotoGallery() {
   return (
-    <div>PhotoGallery</div>
+    <main className="flex flex-col items-center pt-12 h-screen">
+      <h1 className='font-bold text-7xl text-[#146C94]'>Photo Gallery</h1>
+      <h2 className='text-4xl text-[#AFD3E2]'>Here is a collection of photos from everyone, select a person to get started</h2>
+      <People />
+    </main>
   )
 }
