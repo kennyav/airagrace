72f34ee33924bad99a14a926e4ab60b72b1911cf
Koz <kenneth.averna@gmail.com>
2023-11-29 23:20:16 -0800

    added the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='flex flex-col text-center mt-12'>
+      <h1 className='font-bold text-7xl text-[#AFD3E2]'>Photo Gallery</h1>
+      <h2 className='text-4xl text-justify p-10 text-[#146C94]'>
+        I wanted to create this website so you could have a place to look back on all the your memories of being 24.
+        I hope that this year has been memorable for you and I look forward to creating new ones as we grow together. It is nice
+        to have reminders of how far we have come, for me at least, I like to remember the different mindset arcs and try to understand 
+        how each of those added to who I am. I am trying not to get philosophical with this and I think I am doing a 
+        pretty good job towing the line. Regardless, I reached out to most of your friends and asked them to provide
+        photos throughout or you together this year, I haven't seen them all yet but I bet it is cool to see yourself and how loved you are
+        from all these different perspectives. I think I am going to show this to you the morning of your Birthday so here are 
+        a couple final remarks, good morning I will show you all your presents now, even the ones that haven't come in yet, I Love
+        You and Happy Birthday!
+        </h2>
+    </div>
   )
 }
