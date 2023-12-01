49c2655b5c02d2cab4ab9299a7fd3a490183a7be
Koz <kenneth.averna@gmail.com>
2023-12-01 15:22:04 -0800

    pablo photos

--- src/components/People/Pablo.jsx
+++ src/components/People/Pablo.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Test from '../Polaroids/Test'
-import PHOTOS from '../Polaroids/photos.json'
+import PHOTOS from '../Polaroids/pablo.json'
 
 export default function Pablo() {
    return (
-      <Test cards={PHOTOS} />
+      <Test cards={PHOTOS} name={'pablo'}/>
    )
 }
