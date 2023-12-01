5a6ef89186438e72a277053f4bd4dffcb9f3a55f
Koz <kenneth.averna@gmail.com>
2023-12-01 13:51:23 -0800

    megan and nicks photos

--- src/components/People/Megan.jsx
+++ src/components/People/Megan.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Test from '../Polaroids/Test'
-import PHOTOS from '../Polaroids/photos.json'
+import PHOTOS from '../Polaroids/megan.json'
 
 export default function Megan() {
    return (
-      <Test cards={PHOTOS} />
+      <Test cards={PHOTOS} name={'megan'}/>
    )
 }
--- src/components/People/Nick.jsx
+++ src/components/People/Nick.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Test from '../Polaroids/Test'
-import PHOTOS from '../Polaroids/photos.json'
+import PHOTOS from '../Polaroids/nick.json'
 
 export default function Nick() {
    return (
-      <Test cards={PHOTOS} />
+      <Test cards={PHOTOS} name={'nick'}/>
    )
 }
